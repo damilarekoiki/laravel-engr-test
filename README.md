591b7a23bf2c346d1c54bda992e5d8891e35719a
Damilare Koiki <koikidamilare@gmail.com>
2024-12-13 00:09:18 +0100

    Read me

--- README.md
+++ README.md
@@ -17,5 +17,16 @@ The UI is displayed on the root page
 
 ## Extra Notes
 
+The BatchClaimsCommand is the command that is meant to run daily to create claim batches
+
+## Batching Algorithm
+
+
+1. Fetch claims that have not been processed, sorted by total amount, priority level, cost of the specialty
+2. Loop through the claims
+3. Claims that belong to the same insurer are sorted by preferred batching date
+4. Before a claim is added to batch, the insurer's daily processing capacity and maximum batch is checked
+5. The daily processing capacity and maximum batch determine whether the claim will be added to batch or batch of next day
+
 
 
--- app/Repositories/ClaimRepository.php
+++ app/Repositories/ClaimRepository.php
@@ -60,26 +60,6 @@ class ClaimRepository extends AbstractRepository implements ClaimInterface {
 
         return $batchedClaims;
 
-        // TODO: Add these comments to the Readme
-
-        // Loop through claims
-
-        // If numberOfBatches for the insurrer in $number_of_batches_made_for_insurer
-        // equals $claim->insurer_maximum_batch_size, we skip the batching for the claim
-
-        // If the numberOfClaims in $claims_batched_for_insurer
-        // equals $claim->daily_processing_capacity, we put the claim in the next day batch
-
-        // Check the $claims_batched_for_provider, if there is a claim with
-        // same insurer_id, total_amount, priority_level, insurer_specialty_percent_cost and
-        // submission_date > $claim->submission_date, we put claim in front of it, else put claim
-        // at the end of $claims_batched_for_provider
-
-        // when we add place a claim in $claims_batched_for_provider, we chack the last claim with
-        // the same indentifier meets the daily_processing_capacity and maximum_batch_size criteria
-        // If it fails numberOfBatches, we skip.
-        // If it fails daily_processing_capacity, we put the claim in the next day batch
-
 
     }
 }
